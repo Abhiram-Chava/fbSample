3acf7977658ab9eb985311ad863ecdded608cb28
abhiram-chava <abhirammetaqa@gmail.com>
2024-09-02 00:28:15 -0700

    Meta

--- src/pack1/Basics.java
+++ src/pack1/Basics.java
@@ -6,6 +6,10 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.testng.Assert;
 
 import files.ReusableMethods;
@@ -13,12 +17,13 @@ import files.payload;
 
 public class Basics {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
+		.body(payload.AddPlace())//new String(Files.readAllBytes(Paths.get("C:\\Users\\abhiramchava\\Downloads\\Library+API.postman_collection.json")))
+		.when().post("maps/api/place/add/json")
 		.then().statusCode(200).body("scope", equalTo("APP"))
 		.header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		
